27b498ef380e55e8a9081f836cd51a6f23a1fb69
shreyansh1910 <shreyansh.maurya.cd.mec20@itbhu.ac.in>
2023-05-08 01:48:15 +0530

    added tmp

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const myRemoveBgFunction = require('./api.js');
 
 const app = express();
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'tmp/' });
 app.use(cors());
 
 app.post('/api/upload', upload.single('image'), async (req, res) => {
@@ -14,9 +14,9 @@ app.post('/api/upload', upload.single('image'), async (req, res) => {
     console.log(req.file);
     const imageFile = req.file;
     console.log(__dirname + '/' + imageFile.path, __dirname + '/' + imageFile.path + 'removed');
-    //  await myRemoveBgFunction(__dirname + '/' + imageFile.path, __dirname + '/' + imageFile.path + 'removed');
+     await myRemoveBgFunction(__dirname + '/' + imageFile.path, __dirname + '/' + imageFile.path + 'removed');
     console.log("done");
-    res.sendFile(__dirname + '/' + "uploads/hello.png");
+    res.sendFile(__dirname + '/' + imageFile.path + 'removed');
 
 });
 

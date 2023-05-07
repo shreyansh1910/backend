0b47c663fca10c286a5bd699328e8d4125133be6
shreyansh1910 <shreyansh.maurya.cd.mec20@itbhu.ac.in>
2023-05-08 03:06:14 +0530

    vrt

--- app.js
+++ app.js
@@ -8,31 +8,22 @@ const app = express();
 
 const upload = multer({ dest: 'tmp' });
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://frontend-orcin-beta.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://frontend-orcin-beta.vercel.app');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+//   });
 
-const corsOptions = {
-    origin: 'https://frontend-orcin-beta.vercel.app', // Replace with your frontend's origin
-    methods: ['GET', 'POST'], // Specify the allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed request headers
-};
+// const corsOptions = {
+//     origin: 'https://frontend-orcin-beta.vercel.app', // Replace with your frontend's origin
+//     methods: ['GET', 'POST'], // Specify the allowed HTTP methods
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed request headers
+// };
 
 //Enable CORS with the specified options
-app.use(cors(corsOptions));
+app.use(cors());
 
-// Enable CORS for all HTTP methods
-// app.use(cors());
-
-// Enable CORS for only specific HTTP methods
-// app.use(cors({
-//     origin: 'https://frontend-orcin-beta.vercel.app',
-//     methods: ['GET', 'POST'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// };
 
 
 app.post('/api/upload', upload.single('image'), async (req, res) => {
@@ -40,7 +31,7 @@ app.post('/api/upload', upload.single('image'), async (req, res) => {
     console.log(req.file);
     const imageFile = req.file;
     console.log(__dirname + '/' + imageFile.path, __dirname + '/' + imageFile.path + 'removed');
-    await myRemoveBgFunction(__dirname + '/' + imageFile.path, __dirname + '/' + imageFile.path + 'removed');
+    await myRemoveBgFunction(__dirname   + '/'+imageFile.path, __dirname  + '/'+imageFile.path + 'removed');
     console.log("done");
     res.sendFile(__dirname + '/' + imageFile.path + 'removed');
 

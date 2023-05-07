7315daa195f366121ac9f0449496751c31a9dfd2
shreyansh1910 <shreyansh.maurya.cd.mec20@itbhu.ac.in>
2023-05-08 01:08:52 +0530

    added vercel

--- app.js
+++ app.js
@@ -7,21 +7,22 @@ const myRemoveBgFunction = require('./api.js');
 const app = express();
 app.use(express.static(__dirname + "/uploads"));
 const upload = multer({ dest: 'uploads/' });
-app.use(cors()); 
+app.use(cors());
 
 app.post('/api/upload', upload.single('image'), async (req, res) => {
     console.log(req.file);
-  const imageFile = req.file;
-  console.log(__dirname + '/' + imageFile.path,__dirname + '/' + imageFile.path+'removed');
-  await myRemoveBgFunction(__dirname + '/' + imageFile.path,__dirname + '/' + imageFile.path+'removed');
-  res.sendFile(__dirname + '/' + imageFile.path+'removed');
- 
+    const imageFile = req.file;
+    console.log(__dirname + '/' + imageFile.path, __dirname + '/' + imageFile.path + 'removed');
+    await myRemoveBgFunction(__dirname + '/' + imageFile.path, __dirname + '/' + imageFile.path + 'removed');
+    res.sendFile(__dirname + '/' + imageFile.path + 'removed');
+
 });
 
+app.get("/", (req, res) => { res.send("BACKEND WORKING") });
 
 
 
 
 app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+    console.log('Server is running on port 3001');
 });
